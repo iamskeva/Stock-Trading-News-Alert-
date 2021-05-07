a984466b55f0cf580de4a7c82861dd0a6e61fc5a
Donatus Prince <75095350+amdonatusprince@users.noreply.github.com>
2021-05-07 17:12:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -84,14 +84,5 @@ if abs(percentage_diffrence) > 0:
         )
 
         print(message.status)
-
-# Optional: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+        
+        
